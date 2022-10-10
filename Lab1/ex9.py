773d705d2479d50dda1bfbc468ba1b9724908912
CapsaDragosGabriel <79151220+CapsaDragosGabriel@users.noreply.github.com>
2022-10-10 15:10:19 +0300

    adaugat la dictionar

--- Lab1/ex9.py
+++ Lab1/ex9.py
@@ -1,13 +1,35 @@
-string="an Apple is not a tomato"
+string="an Applesssss is not a tomato"
 string=string.lower()
 appearences={
     "a":0,
     "e":0,
     "i":0,
     "o":0,
-    "u":0
+    "u":0,
+    "b":0,
+    "c":0,
+    "d":0,
+    "f":0,
+    "g":0,
+    "h":0,
+    "j":0,
+    "l":0,
+    "m":0,
+    "n":0,
+    "p":0,
+    "k":0,
+    "q":0,
+    "r":0,
+    "s":0,
+    "t":0,
+    "v":0,
+    "x":0,
+    "y":0,
+    "z":0,
+    "w":0
+
 }
-vowels="aeiou"
+vowels="abcdefhijklmnopqrstuvwxyz"
 for i in string:
     if vowels.count(i):
         appearences[i]=appearences[i]+1
